33e17a34920450c16c59fe38a246941e0193240f
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-01-02 15:16:29 +0100

    feat: implement signup server action and enhance auth form with error handling

---
+++ actions/auth-actions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+export type SignupActionState = {
+  errors?: { email?: string; password?: string };
+};
+
+export const signup = async (
+  prevState: SignupActionState,
+  formData: FormData
+): Promise<SignupActionState> => {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  const errors: typeof prevState.errors = {};
+
+  if (!email || !password) {
+    errors.email = "Email and password are required";
+  }
+
+  if (!email.includes("@")) {
+    errors.email = "Email must contain an @ symbol";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 characters";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  return { errors: undefined };
+};
--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -1,8 +1,20 @@
-import Link from 'next/link';
+"use client";
+
+import Link from "next/link";
+
+import { signup, SignupActionState } from "@/actions/auth-actions";
+import { useActionState } from "react";
 
 export const AuthForm = () => {
+  const [{ errors }, formAction] = useActionState<SignupActionState, FormData>(
+    signup,
+    {
+      errors: { email: "", password: "" },
+    }
+  );
+
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
@@ -14,14 +26,19 @@ export const AuthForm = () => {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </p>
+      {errors && (
+        <ul id="form-errors">
+          {Object.keys(errors).map((key) => (
+            <li key={key}>{errors[key as keyof typeof errors]}</li>
+          ))}
+        </ul>
+      )}
       <p>
-        <button type="submit">
-          Create Account
-        </button>
+        <button type="submit">Create Account</button>
       </p>
       <p>
         <Link href="/">Login with existing account.</Link>
       </p>
     </form>
   );
-}
+};
