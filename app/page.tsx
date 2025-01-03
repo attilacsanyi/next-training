114d14b17218127cb170526f478d62cf523d3588
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-01-03 17:31:10 +0100

    feat: enhance environment configuration and logging for development and production modes

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,10 @@ import { AuthForm } from "@/components/auth-form";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ mode: 'login' | 'signup' }> }) {
   const {mode = 'login'} = await searchParams;
+
+  if (process.env.ENV === "development") {
+    console.log('This is a development environment');
+  }
+  
   return <AuthForm mode={mode} />;
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+type Envs = {
+  ENV: string;
+};
+
+const nextConfig = (phase: string, config: NextConfig): NextConfig => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    config.env = {
+      ...config.env,
+      ENV: "development",
+    } satisfies Envs;
+  }
+
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    config.env = {
+      ...config.env,
+      ENV: "production",
+    } satisfies Envs;
+  }
+
+  return config;
 };
 
 export default nextConfig;
